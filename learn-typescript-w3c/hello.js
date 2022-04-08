c6bbbae67e94aef4edbee074b78f79989be3e903
jmreis <jmsreis@protonmail.com>
2022-04-07 23:43:32 -0300

    Add typescript language

---
+++ learn-typescript-w3c/hello.js
@@ -0,0 +1,3 @@
+var firstname = "Dylan"; // type string
+console.log("hello world");
+console.log(typeof firstname);
---
+++ learn-typescript-w3c/hello.ts
@@ -0,0 +1,3 @@
+let firstname: string = "Dylan";		// type string
+console.log("hello world");
+console.log(typeof firstname);
