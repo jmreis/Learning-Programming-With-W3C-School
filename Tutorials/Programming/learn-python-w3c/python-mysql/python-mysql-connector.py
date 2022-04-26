ae3f4ead7f8631830adaaa1a31ad3d9873aaee27
jmreis <jmsreis@protonmail.com>
2022-04-25 23:52:42 -0300

    Update python code

--- Tutorials/Programming/learn-python-w3c/python-mysql/python-mysql-connector.py
+++ Tutorials/Programming/learn-python-w3c/python-mysql/python-mysql-connector.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 # Demo of mysql connection
 
-mydb = mysql.connector.connect(
-        host="localhost",
-        user="test",
-        password="test123"
-    )
-print(mydb)
+
+def main():
+
+    mydb = mysql.connector.connect(
+            host="localhost",
+            user="test",
+            password="test123"
+        )   
+    print(mydb)
+
+
+if __name__ == "__main__":
+    main()
